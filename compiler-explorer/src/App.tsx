cb5a199c6e71bcb572bf5234d5e09bbf3252c88a
Jorik Schellekens <joriksch@gmail.com>
2021-07-21 18:27:29 +0100

    Write quick frontend

--- compiler-explorer/src/App.tsx
+++ compiler-explorer/src/App.tsx
@@ -1,26 +1,91 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { useDebounce } from 'use-debounce';
+import Editor from "@monaco-editor/react";
 import './App.css';
 
 function App() {
+  const [volatileSource, setSource] = useState(defaultProgram);
+  const [source] = useDebounce(volatileSource, 1000);
+
+  function handleEditorDidMount(editor, monaco) {
+    editorRef.current = editor; 
+  }
+
+//   if (editorRef.current !== null) {
+//     editorRef.current.deltaDecorations([], [
+//       { range: new monaco.Range(3,1,5,1), options: { isWholeLine: true, linesDecorationsClassName: 'myLineDecoration' }},
+//     ]);
+//   }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="editorWrapper">
+        <h>Slang</h>
+        <Editor
+          className="editor"
+          height="100vh"
+          defaultLanguage="javascript"
+          defaultValue={source}
+          onChange={(value, event) => value === undefined ? setSource("") : setSource(value)}
+          options={{theme: "vs-dark"}}
+        />
+      </div>
+      <div className="editorWrapper">
+        <h>Interpreter 2</h>
+        <Editor
+          height="100vh"
+          className="editor"
+          defaultLanguage="javascript"
+          defaultValue="// some comment"
+          value={clean(slang.interp2(source))}
+          options={{readOnly: true}}
+        />
+      </div>
+      <div className="editorWrapper">
+        <h>Interpreter 3</h>
+         <Editor
+          height="100vh"
+          className="editor"
+          defaultLanguage="javascript"
+          defaultValue="// some comment"
+          value={clean(slang.interp3(source))}
+          options={{readOnly: true}}
+        />
+      </div>
+      <div className="editorWrapper">
+        <h>Jargon</h>
+        <Editor
+          height="100vh"
+          defaultLanguage="javascript"
+          className="editor"
+          defaultValue="// some comment"
+          value={clean(slang.jargon(source))}
+          options={{readOnly: true}}
+        />
+      </div>
     </div>
   );
 }
 
+const clean = s =>
+  s.split("\n")
+    // .map(s => s.replace(/^\t/, ''))
+    // .map(s => s.replace(/^\s/, ''))
+    // .map(s => s.replace(/\[/, ''))
+    // .map(s => s.replace(/\]/, ''))
+    // .map(s => s.replace(/\;/, ''))
+    // .filter(s => s !== "")
+    .join("\n")
+
+const defaultProgram = `let fib( m : int) : int = 
+    if m = 0
+    then 1 
+    else if m = 1 
+         then 1 
+         else fib (m - 1) + fib (m -2) 
+         end 
+    end 
+in 
+    fib(1) 
+end`
 export default App;
--- web/export.ml
+++ web/export.ml
@@ -1,13 +1,17 @@
 open Js_of_ocaml
 open Slang
 
+let wrap interp str =
+  try interp str
+  with 
+    | Errors.Error s -> s
 
 let frontend str = Front_end.front_end_from_string (Js.to_string str)
 let _ =
   Js.export "slang"
     (object%js
-       method interp0      str = Js.string (Interp_0.string_of_value (Interp_0.interpret_top_level (frontend str)))
-       method interp2_code str = Js.string (Interp_2.string_of_code  (Interp_2.compile (frontend str)))
-       method interp3_code str = Js.string (Interp_3.string_of_code  (Interp_3.compile (frontend str)))
-       method jargon_code  str = Js.string (Jargon.string_of_listing (Jargon.compile (frontend str)))
+       method interp0      str = Js.string (wrap (fun x -> (Interp_0.string_of_value (Interp_0.interpret_top_level (frontend x)))) str)
+       method interp2_code str = Js.string (wrap (fun x -> (Interp_2.string_of_code  (Interp_2.compile (frontend x)))) str)
+       method interp3_code str = Js.string (wrap (fun x -> (Interp_3.string_of_code  (Interp_3.compile (frontend x)))) str)
+       method jargon_code  str = Js.string (wrap (fun x -> (Jargon.string_of_listing (Jargon.compile (frontend x)))) str)
      end)
