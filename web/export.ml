a4874ef4724952810b41fce4601acf8df25308ec
Jorik Schellekens <joriksch@gmail.com>
2021-08-31 16:17:24 +0100

    Reset compiler labels for each compile in JavaScript

--- web/export.ml
+++ web/export.ml
@@ -12,6 +12,6 @@ let _ =
     (object%js
        method interp0      str = Js.string (wrap (fun x -> (Interp_0.string_of_value (Interp_0.interpret_top_level (frontend x)))) str)
        method interp2_code str = Js.string (wrap (fun x -> (Interp_2.string_of_code  (Interp_2.compile (frontend x)))) str)
-       method interp3_code str = Js.string (wrap (fun x -> (Interp_3.string_of_code  (Interp_3.compile (frontend x)))) str)
-       method jargon_code  str = Js.string (wrap (fun x -> (Jargon.string_of_listing (Jargon.compile (frontend x)))) str)
+       method interp3_code str = Js.string (wrap (fun x -> (Interp_3.reset(); (Interp_3.string_of_code  (Interp_3.compile (frontend x))))) str)
+       method jargon_code  str = Js.string (wrap (fun x -> (Jargon.reset(); Jargon.string_of_listing (Jargon.compile (frontend x)))) str)
      end)
